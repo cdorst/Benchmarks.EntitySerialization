ce94565aa73dd3ea0a43baa0ff2ebe47d8539660
Christopher Dorst <chris@dorst.email>
2018-04-22 09:47:46 -0500

    Add benchmarks

---
+++ JsonBenchmarks/Entity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace JsonBenchmarks
+{
+    public class Entity
+    {
+        public static Entity Default = new Entity();
+
+        public int EntityId { get; set; } = 1_000_000;
+
+        public Foo ForeignKeyOne { get; set; } // Assume that reference objects are not loaded
+        public int ForeignKeyOneId { get; set; } = 1_000_001;
+
+        public Foo ForeignKeyTwo { get; set; } // Assume that reference objects are not loaded
+        public int ForeignKeyTwoId { get; set; } = 1_000_002;
+
+        public override string ToString()
+            => new StringBuilder().Append("{\"entityId\":").Append(EntityId).Append(",\"foreignKeyOneId\":").Append(ForeignKeyOneId).Append(",\"foreignKeyTwoId\":").Append(ForeignKeyTwoId).Append(',').ToString();
+
+        /// <summary>
+        /// Special-case to be used as a plaintext response in an ASP.NET Core API environment;
+        /// Assumes:
+        ///     Requesting client knows how to construct object from the resulting string
+        ///     Requesting client already knows the primary-key value
+        /// </summary>
+        public string ToStringCsv()
+            => new StringBuilder().Append(ForeignKeyOneId).Append(',').Append(ForeignKeyTwoId).ToString();
+
+        public byte[] ToBytes()
+        {
+            var result = new byte[8];
+            Buffer.BlockCopy(new[] { ForeignKeyOneId, ForeignKeyTwoId }, 0, result, 0, 8);
+            return result;
+        }
+    }
+
+    public class MixedKeyTypes
+    {
+        public static MixedKeyTypes Default = new MixedKeyTypes();
+
+        public long ForeignKeyOneId { get; set; } = 1_000_000_000_000;
+        public int ForeignKeyTwoId { get; set; } = 1_000_000;
+        public short ForeignKeyThreeId { get; set; } = 1_000;
+        public byte ForeignKeyFourId { get; set; } = 100;
+
+        public byte[] ToBytes()
+        {
+            var result = new byte[32];
+            Buffer.BlockCopy(new[] { ForeignKeyOneId, ForeignKeyTwoId, ForeignKeyThreeId, ForeignKeyFourId }, 0, result, 0, 32);
+            return result;
+        }
+    }
+
+    public class Foo
+    {
+        public int FooId { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
--- JsonBenchmarks/Program.cs
+++ JsonBenchmarks/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using Newtonsoft.Json;
+using System;
 
 namespace JsonBenchmarks
 {
-    class Program
+    [CoreJob]
+    [RPlotExporter, RankColumn]
+    public class JsonTests
     {
-        static void Main(string[] args)
+        [Benchmark]
+        public byte[] BlockCopyIntegerBytes() => Entity.Default.ToBytes();
+        [Benchmark]
+        public byte[] BlockCopyMixedBytes() => MixedKeyTypes.Default.ToBytes();
+        [Benchmark]
+        public string StringBuilderCsv() => Entity.Default.ToStringCsv();
+        [Benchmark]
+        public string JilJson() => Jil.JSON.Serialize(Entity.Default);
+        [Benchmark]
+        public string StringBuilderJson() => Entity.Default.ToString();
+        [Benchmark]
+        public string NewtonsoftJson() => JsonConvert.SerializeObject(Entity.Default);
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<JsonTests>();
+            var csvString = Entity.Default.ToStringCsv();
+            var csvLength = csvString.Length;
+            var jsonString = Entity.Default.ToString();
+            var jsonLength = jsonString.Length;
+            Console.WriteLine($"CSV string: {csvString}");
+            Console.WriteLine($"JSON string: {jsonString}");
+            Console.WriteLine($"Length of CSV string: {csvLength}");
+            Console.WriteLine($"Length of JSON string: {jsonLength}");
+            Console.WriteLine($"CSV string contains {((decimal)csvLength / jsonLength).ToString("p")} as much data as the JSON representation");
         }
     }
 }
